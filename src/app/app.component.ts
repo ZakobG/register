356fd708781ae47331ef7304278a5417e4cb9186
ZakobG <59724611+ZakobG@users.noreply.github.com>
2021-03-14 18:50:45 +0100

    Hope it works!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'validace';
+
+  log(x) {
+    console.log(x);
+  }
 }
